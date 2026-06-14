fa7f2c4d4b4c0a75d8f154b74c5ea6c1c7d6d88b
selvnv <selvnv@post-inform.ru>
2026-06-14 13:16:32 +0700

    Add favicon

--- build_cookbooks.py
+++ build_cookbooks.py
@@ -316,6 +316,7 @@ def build_cookbook(style, output_path, recipe_dirs):
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="favicon.png">
     <title>{title}</title>
     <style>
 {base_css}
